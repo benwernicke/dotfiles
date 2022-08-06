0ad95ac3063d2b5f140d73ca658490e62c34fb69
= <=>
2022-08-06 23:22:33 +0200

    dwmblocks

---
+++ dwmblocks/blocks.h
@@ -0,0 +1,16 @@
+// Modify this file to change what commands output to your statusbar, and
+// recompile using the make command.
+static const Block blocks[] = {
+    /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
+    { " ", "", 6000, 0 },
+    { "", "sensors | awk '/Core 0/ {print $3}'", 10, 0 },
+    { "", "pfetch | grep 'memory' | grep -Eo '[0-9]?[0-9]{3}M / ....M'", 30,
+        0 },
+    { "", "clock", 1, 0 },
+    { "", "battery", 60, 0 },
+};
+
+// sets delimeter between status commands. NULL character ('\0') means no
+// delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;

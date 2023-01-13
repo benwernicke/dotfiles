6537f4b92fbddac8b4819e6a30053736fa8bf025
= <=>
2023-01-13 01:08:44 +0100

    removed more stuff

--- dwm/dwm.c
+++ dwm/dwm.c
@@ -109,7 +109,7 @@ typedef struct {
 
 typedef struct {
 	const char * sig;
-	void (*func)(const Arg *);
+	void (*func)(void);
 } Signal;
 
 typedef struct {
@@ -1036,10 +1036,8 @@ fake_signal(void)
 	char fsignal[256];
 	char indicator[9] = "fsignal:";
 	char str_sig[50];
-	char param[16];
-	int i, len_str_sig, n, paramn;
+	int i, len_str_sig;
 	size_t len_fsignal, len_indicator = strlen(indicator);
-	Arg arg;
 
 	// Get root name property
 	if (gettextprop(root, XA_WM_NAME, fsignal, sizeof(fsignal))) {
@@ -1047,22 +1045,11 @@ fake_signal(void)
 
 		// Check if this is indeed a fake signal
 		if (len_indicator > len_fsignal ? 0 : strncmp(indicator, fsignal, len_indicator) == 0) {
-			paramn = sscanf(fsignal+len_indicator, "%s%n%s%n", str_sig, &len_str_sig, param, &n);
-
-			if (paramn == 1) arg = (Arg) {0};
-			else if (paramn > 2) return 1;
-			else if (strncmp(param, "i", n - len_str_sig) == 0)
-				sscanf(fsignal + len_indicator + n, "%i", &(arg.i));
-			else if (strncmp(param, "ui", n - len_str_sig) == 0)
-				sscanf(fsignal + len_indicator + n, "%u", &(arg.ui));
-			else if (strncmp(param, "f", n - len_str_sig) == 0)
-				sscanf(fsignal + len_indicator + n, "%f", &(arg.f));
-			else return 1;
 
 			// Check if a signal was found, and if so handle it
 			for (i = 0; i < LENGTH(signals); i++)
 				if (strncmp(str_sig, signals[i].sig, len_str_sig) == 0 && signals[i].func)
-					signals[i].func(&(arg));
+					signals[i].func();
 
 			// A fake signal was sent
 			return 1;
